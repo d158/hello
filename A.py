ef33bc33ac426b0e1d1c6109ca93fe85106e0ebc
d158 <54879768+d158@users.noreply.github.com>
2019-10-24 18:22:27 +0530

    Add files via upload

---
+++ A.py
@@ -0,0 +1,13 @@
+
+x = 'this is Dileep Kumar, Iam writing a program.This is not a copy writer'
+v = ('aeiou')
+count = 0
+for letters in x:
+    if letters in v:
+     count +=1
+
+print('vowles:',count)
+
+
+
+
